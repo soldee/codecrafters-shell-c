671cc0e184541d0eee3f881b1d7b67e7009018ac
soldee <71665509+soldee@users.noreply.github.com>
2024-06-16 19:48:11 +0200

    print command not found if it is not recognized

--- app/main.c
+++ app/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
-int main() {
-  printf("$ ");
-  fflush(stdout);
+void remove_trailing_newline(char* str) {
+	int i = 0;
+	while (str[i] != '\n' && str[i] != '\0') {
+		i++;
+	}
+	if (str[i] == '\n') {
+		str[i] = '\0';
+	}
+}
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  return 0;
+
+int main() {
+	printf("$ ");
+	fflush(stdout);
+
+	// Wait for user input
+	char input[100];
+	fgets(input, 100, stdin);
+
+	remove_trailing_newline(input);
+
+	printf("%s: command not found\n", input);
+	fflush(stdout);
+
+	return 0;
 }
