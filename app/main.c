f19ef1246c5a1643c2aac1be5d2dcf950ec1e96c
soldee <71665509+soldee@users.noreply.github.com>
2024-06-16 20:45:23 +0200

    support exit function

--- app/main.c
+++ app/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void remove_trailing_newline(char *str) {
     int i = 0;
@@ -7,6 +9,14 @@ void remove_trailing_newline(char *str) {
     }
     if (str[i] == '\n') {
         str[i] = '\0';
+    }
+}
+
+void read_str_until_space(char *input, char *read) {
+    int i = 0;
+    while (input[i] != ' ' && input[i] != '\0') {
+        read[i] = input[i];
+        i++;
     }
 }
 
@@ -20,9 +30,20 @@ int main() {
 
         remove_trailing_newline(input);
 
-        printf("%s: command not found\n", input);
-        fflush(stdout);
+        char *cmd;
+        cmd = strtok(input, " ");
+
+        if (strcmp(cmd, "exit") == 0) {
+            char *exit_code_str;
+            exit_code_str = strtok(NULL, " ");
+            int exit_code = atoi(exit_code_str); // atoi returns 0 if an invalid string is returned
+
+            return exit_code;
+        } else {
+            printf("%s: command not found\n", input);
+            fflush(stdout);
+        }
     }
 
-	return 0;
+    return 0;
 }
