c88f914996accb5bd7c4d5e250b85947de453dc5
osoldevila <osoldevila@minsait.com>
2024-06-17 21:10:23 +0200

    add support for echo command

--- app/main.c
+++ app/main.c
@@ -3,47 +3,52 @@
 #include <string.h>
 
 void remove_trailing_newline(char *str) {
-    int i = 0;
-    while (str[i] != '\n' && str[i] != '\0') {
-        i++;
-    }
-    if (str[i] == '\n') {
-        str[i] = '\0';
-    }
+  int i = 0;
+  while (str[i] != '\n' && str[i] != '\0') {
+    i++;
+  }
+  if (str[i] == '\n') {
+    str[i] = '\0';
+  }
 }
 
 void read_str_until_space(char *input, char *read) {
-    int i = 0;
-    while (input[i] != ' ' && input[i] != '\0') {
-        read[i] = input[i];
-        i++;
-    }
+  int i = 0;
+  while (input[i] != ' ' && input[i] != '\0') {
+    read[i] = input[i];
+    i++;
+  }
 }
 
 int main() {
-    while (1) {
-        printf("$ ");
+  while (1) {
+    printf("$ ");
+    fflush(stdout);
+
+    char input[100];
+    fgets(input, 100, stdin);
+
+    remove_trailing_newline(input);
+
+    char *cmd;
+    cmd = strtok(input, " ");
+
+    if (strcmp(cmd, "exit") == 0) {
+      char *exit_code_str;
+      exit_code_str = strtok(NULL, " ");
+      int exit_code = atoi(exit_code_str);  // atoi returns 0 if an invalid string is returned
+      return exit_code;
+
+    } else if (strcmp(cmd, "echo") == 0) {
+        char* echoString = input; 
+        echoString += 5; // add len("echo ") to pointer
+        printf("%s\n", echoString);
+
+    } else {
+        printf("%s: command not found\n", input);
         fflush(stdout);
-
-        char input[100];
-        fgets(input, 100, stdin);
-
-        remove_trailing_newline(input);
-
-        char *cmd;
-        cmd = strtok(input, " ");
-
-        if (strcmp(cmd, "exit") == 0) {
-            char *exit_code_str;
-            exit_code_str = strtok(NULL, " ");
-            int exit_code = atoi(exit_code_str); // atoi returns 0 if an invalid string is returned
-
-            return exit_code;
-        } else {
-            printf("%s: command not found\n", input);
-            fflush(stdout);
-        }
     }
-
-    return 0;
+  }
+  
+  return 0;
 }
